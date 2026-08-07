1c4b997078b03925bb2289a8024ba90dfcc4cea2
sainair <sai150306@gmail.com>
2026-08-07 11:30:59 +0300

    css fixes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,7 +12,7 @@ interface CardProps{
 const Card = ({heading, body, topContent, children, className}:CardProps) => {
   return (
     <div className={`card ${className}`.trim()} style={{width: "18rem"}}>
-        <div className={`card-body ${className}`}>
+        <div className={`card-body`}>
             {topContent}
             <h5 className='card-title'>{heading}</h5>
             <p className='card-text'>{body}</p>
